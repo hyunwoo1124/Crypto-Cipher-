3b344ec8304c16ae8d5d143343ef007e1f89b898
student <quyenhua137@gmail.com>
2020-03-04 22:00:21 -0800

    Working Iteration

--- cipher.py
+++ cipher.py
@@ -2,6 +2,7 @@
 
 #parser that allows cmd line to take argument into the program
 import argparse
+#from itertools import cycle
 #while (!success) { success = try() }
 # that up there gents is the key to get a job at FANG
 #Implementing flags...
@@ -23,77 +24,96 @@ def main():
 # Definitions to be implemented...
 
 def info():
-    print("The Classic 6 Cryptography Ciphers\n")
+    print("The Classic 5 Cryptography Ciphers\n")
     print("Cipher Name: Is the name of the cipher. Valid names are:\n")
     print("\t -PLF: Playfair\n")
     print("\t -RTS: Row Trasposition\n")
     print("\t -RFC: Railfence\n")
     print("\t -VIG: Vigenre\n")
     print("\t -CES: Caesar\n")
-    print("\t -MAC: Monoalphabetic\n")
     print("\nFormat: ./cipher  -C <CIPHERNAME> -K <KEY> -T <ENC/DEC>  -I <INPUTFILE> -O <OUTPUTFILE>\n")
     print("\nUse the flag -h to get help if needed...\n")
 
 # This module takes the plaintext and key to encrypt into ciphertext
 def RailfenceEnc(plaintext, key):
+
     result = ""
 
     #Creating a Matrix with for loop to expand the matrix
-    matrix = [["\n" for x in range(len(plaintext))] for y in range(key)]
+    #matrix = [["\n" for x in range(len(plaintext))] for y in range(key)]
     
-    # intiail variable to further use for the next code
-    increment = 1
-    row, col = 0, 0
+    # The key index
+    keyOffset = 0
+    
+    index = 0
+    
+    intKey = int(key)
 
-    for character in plaintext:
-        if row + increment < 0 or row + increment >= len(matrix):
-            increment = increment * -1
-        matrix[row][col] = character
-        # add the increment for each row and col
-        row += increment
-        col += 1
-    for list in matrix:
-        #connecting each character one by one from the matrix; concatenating
-        result += "".join(list)
-    return result 
+    #key = int(args.Key)
+    #key = 3
+    #plaintext = "helloworld"
+    while keyOffset < intKey:
+               
+        index = keyOffset
+
+        while index < len(plaintext):
+        
+           if len(plaintext) > index:
+               result += plaintext[index]
+
+               index += intKey
+        
+        keyOffset += 1
+        index = 0
+
+    return result
+   
 # This module takes the ciphertext and key to decrypt into plaintext
 def RailfenceDec(ciphertext, key):
-    #Since we dont konw the  exact key at the moment we need
-    #Boolean function to determine if its end of the row or 
-    #the place value of the column
 
-    result = []
-    #The Matrix for the railfence decrypt
-    matrix= [["" for x in range(len(ciphertext))] for y in range(key)]
+    #length of key
+    # should be ciphertext // key but ours have an input key section
+    keyLength = int(key)
+    key = int(key)
 
-    #Finding the correct direction of the position
-    #initialize the Boolena and the position of row, col
-    row, col = 0, 0
-    goingDown = False
+    #Remainder of key length with modulus
+    keyRemainder = len(ciphertext) % key
 
-    for character in range(len(ciphertext)):
-        #checking to see if the row is filled or not to the end
-        if(row == 0) or (row == key-1):
-            #checking the direction of the flow 
-            # is it 0? 1 ,2 ,3? (row)
-            goingDown = not goingDown
+    keylengths = key * [keyLength]
 
-            #appending the characters
-            matrix[row,col] = ciphertext[character]
-            col += 1
+    for i in range(0, keyRemainder):
+        keylengths[i] += 1
+    keylengths.reverse()
 
-            if goingDown:
-                #Going to the next row incrementing
-                row +=1
-            else:
-                #If not go up the row
-                row -=1
-    for col in range(key):
-        for row in range(len(ciphertext)):
-            if matrix[col, row] != "\n":
-                result.append(matrix[col][row])
-    return ("".join(result))
+    rails =[]
 
+    for length in keylengths:
+        length.append(ciphertext[0:length])
+        ciphertext = ciphertext[length:]
+    rails.reverse()
+
+    string = ''
+
+    for i in range(keylengths + 1):
+        for rail in rails:
+            if len(rail) > i:
+                string += rail[i]
+    return string
+    
+def MonoalphabeticDec(ciphertext, key):
+    key.lower()
+    if len(key) != 26:
+        print ("Please enter 26 key for this cipher")
+
+    Alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    cipherAlphabet = key
+    decryptmessage = []
+    for i in range(len(ciphertext)):
+        for j in range(len(Alphabet)):
+            if  cipherAlphabet[j] == ciphertext[i]:
+                decryptmessage.append(Alphabet[j])
+                break
+    return ("".join(decryptmessage))
 
 def MonoalphabeticEnc(plaintext, key):
     key.lower()
@@ -109,51 +129,32 @@ def MonoalphabeticEnc(plaintext, key):
                 break
     return ("".join(encryptmessage))
 
-def MonoalphabeticDec(ciphertext, key):
-    key.lower()
-    if len(key) != 26:
-        print ("Please enter 26 key for this cipher")
-    Alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    cipherAlphabet = key
-    decryptmessage = []
-    for i in range(len(ciphertext)):
-        for j in range(len(Alphabet)):
-            if  cipherAlphabet[j] == ciphertext[i]:
-                decryptmessage.append(Alphabet[j])
-                break
-    return ("".join(decryptmessage))
 
 def menu_switch(args):
-    
-    if (args.Cipher == 'RFC' and args.Type == 'ENC'):
-        with open(args.Input, 'r') as rf:
-            args.Output = RailfenceEnc(rf, args.Key)
-        with open(args.output, 'a') as wf:
-            print(wf)
-            
-
-    if (args.Cipher == 'RFC' and args.Type == 'DEC'):
+   if (args.Cipher == 'RFC' and args.Type == 'ENC'):
+       with open(args.Input, 'r') as rf:
+           answer = RailfenceEnc(rf.read(), args.Key)
+           with open (args.Output, 'a') as wf:
+               wf.write(answer)
+   if (args.Cipher == 'RFC' and args.Type == 'DEC'):
         with open(args.Input,'r') as rf:
-            RailfenceDec(rf,args.Key)
-        with open(args.Input, 'r') as rf:
-            RailfenceDec(rf,args.Key)
-
-    if (args.Cipher == 'MAC' and args.Type == 'ENC'):
-        with open(args.Input, 'r') as plaintext:
-            args.Out = MonoalphabeticEnc(plaintext, args.Key)
-        with open(args.output, 'a') as ciphertext:
-            print(ciphertext)
-    
-    if (args.Cipher == 'MAC' and args.Type == 'DEC'):
-        with open(args.Input, 'r') as ciphertext:
-            MonoalphabeticDec(cyphertext, args.Key)
-        with open(arge.output, 'a') as plaintext:
-            print(plaintext)
-
-  
-
+            answer = RailfenceDec(rf.read(),args.Key)
+            with open(args.Input, 'a') as wf:
+                wf.write(answer)
+   if (args.Cipher == 'MAC' and args.Type == 'ENC'):
+       with open(args.Input, 'r') as rf:
+           answer = MonoalphabeticEnc(rf.read(), args.Key)
+           with open (args.Output, 'a') as wf:
+               wf.write(answer)
+   if (args.Cipher == 'MAC' and args.Type == 'DEC'):
+        with open(args.Input,'r') as rf:
+            answer = MonoalphabeticDec(rf.read(),args.Key)
+            with open(args.Output, 'a') as wf:
+                wf.write(answer)
+                
 
 if __name__ == '__main__':
     info()
+    # Object of args passed into return value from main def
     args = main()
     menu_switch(args)
--- mono.py
+++ mono.py
@@ -1,3 +1,14 @@
+import argparse
+def main():
+    parser = argparse.ArgumentParser(description="Five Classic Ciphers.")
+    parser.add_argument('-C', '--Cipher', type = str, metavar='', required=True, help ='Choose Cipher: PLF, RTS, RFC, VIG, CES')
+    parser.add_argument('-K', '--Key', type = str, metavar='', required=True, help ='Your Key to encrypt or decrypt')
+    parser.add_argument('-T', '--Type', type = str, metavar='', required=True, help ='Encrypt: ENC or Decrypt: DEC')
+    parser.add_argument('-I', '--Input', type = str, metavar='', required=True, help ='The file from which to read the input.')
+    parser.add_argument('-O', '--Output', type = str, metavar='', required=True, help ='The file to which the output shall be written.')
+    args = parser.parse_args()
+    print(args)
+    return args
 
 def MonoalphabeticDec(ciphertext, key):
     key.lower()
@@ -29,9 +40,23 @@ def MonoalphabeticEnc(plaintext, key):
     return ("".join(encryptmessage))
     
 key  = "fghijklabcdemnopqrstuvwxyz"
-plaintext = "hello dsadasas world"
+plaintext = "helloworld"
 
 encrypted = MonoalphabeticEnc(plaintext,key)
 print (encrypted)
 decrypted = MonoalphabeticDec(encrypted, key)
 print (decrypted)
+
+   if (args.Cipher == 'MAC' and args.Type == 'ENC'):
+       with open(args.Input, 'r') as rf:
+           answer = MonoalphabeticEnc(rf.read(), args.Key)
+           with open (args.Output, 'a') as wf:
+               wf.write(answer)
+
+
+
+   if (args.Cipher == 'MAC' and args.Type == 'DEC'):
+        with open(args.Input,'r') as rf:
+            answer = MonoalphabeticDec(rf.read(),args.Key)
+            with open(args.Output, 'a') as wf:
+                wf.write(answer)
