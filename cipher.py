dbffe9f3ce779e4829eb0a8c5b72d563e9dc00f9
Mike <mikecheng@Mikes-MacBook-Pro.local>
2020-03-09 21:52:48 -0700

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 
 #parser that allows cmd line to take argument into the program
 import argparse
@@ -181,7 +180,145 @@ def vigenereDec(ciphertext,key):
         decString.append(chr(posLetter))
     return ("".join(decString))
    
-   
+def playfairEnc(plaintext, key):
+    pfkey=key
+    pfkey=pfkey.replace(" ", "")
+    pfkey=pfkey.upper()
+    msg=plaintext
+    msg=msg.upper()
+    msg=msg.replace(" ", "")
+    answer=str("Encrypted Text: ")             
+    i=0
+    def matrix(x,y,initial):
+        return [[initial for i in range(x)] for j in range(y)]
+        
+    result=list()
+    #storing key
+    for c in pfkey: 
+        if c not in result:
+            if c=='J':
+                result.append('I')
+            else:
+                result.append(c)
+    flag=0
+    #filling in characters (ASCII)
+    #I/J fixed
+    for i in range(65,91): 
+        if chr(i) not in result:
+            if i==73 and chr(74) not in result:
+                result.append("I")
+                flag=1
+            elif flag==0 and i==73 or i==74:
+                pass    
+            else:
+                result.append(chr(i))
+    k=0
+    #matrix creation
+    my_matrix=matrix(5,5,0) 
+    for i in range(0,5): 
+        for j in range(0,5):
+            my_matrix[i][j]=result[k]
+            k+=1
+    #get location of each character
+    def locindex(c): 
+        loc=list()
+        #making sure J&I are the same
+        if c=='J':
+            c='I'
+        for i ,j in enumerate(my_matrix):
+            for k,l in enumerate(j):
+                if c==l:
+                    loc.append(i)
+                    loc.append(k)
+                    return loc
+        #add X if repeating character
+    for s in range(0,len(msg)+1,2):
+        if s<len(msg)-1:
+            if msg[s]==msg[s+1]:
+                msg=msg[:s+1]+'X'+msg[s+1:]
+    #adding X if message is odd number of characters
+    if len(msg)%2!=0:
+        msg=msg[:]+'X'
+    print("CIPHER TEXT:",end=' ')
+    while i<len(msg):
+        loc=list()
+        loc=locindex(msg[i])
+        loc1=list()
+        loc1=locindex(msg[i+1])
+        if loc[1]==loc1[1]:
+            answer=answer + ("{}{}".format(my_matrix[(loc[0]+1)%5][loc[1]],my_matrix[(loc1[0]+1)%5][loc1[1]]))
+        elif loc[0]==loc1[0]:
+            answer=answer + ("{}{}".format(my_matrix[loc[0]][(loc[1]+1)%5],my_matrix[loc1[0]][(loc1[1]+1)%5]))  
+        else:
+            answer=answer + ("{}{}".format(my_matrix[loc[0]][loc1[1]],my_matrix[loc1[0]][loc[1]]))    
+        i=i+2
+    return answer
+
+def playfairDec(ciphertext, key):
+    pfkey=key
+    pfkey=pfkey.replace(" ", "")
+    pfkey=pfkey.upper()
+    msg=ciphertext
+    msg=msg.upper()
+    msg=msg.replace(" ", "")             
+    i=0
+    def matrix(x,y,initial):
+        return [[initial for i in range(x)] for j in range(y)]
+        
+    result=list()
+    #storing key
+    for c in pfkey: 
+        if c not in result:
+            if c=='J':
+                result.append('I')
+            else:
+                result.append(c)
+    flag=0
+    #filling in characters (ASCII)
+    #I/J fixed
+    for i in range(65,91): 
+        if chr(i) not in result:
+            if i==73 and chr(74) not in result:
+                result.append("I")
+                flag=1
+            elif flag==0 and i==73 or i==74:
+                pass    
+            else:
+                result.append(chr(i))
+    k=0
+    #matrix creation
+    my_matrix=matrix(5,5,0) 
+    for i in range(0,5): 
+        for j in range(0,5):
+            my_matrix[i][j]=result[k]
+            k+=1
+    #get location of each character
+    def locindex(c): 
+        loc=list()
+        #making sure J&I are the same
+        if c=='J':
+            c='I'
+        for i ,j in enumerate(my_matrix):
+            for k,l in enumerate(j):
+                if c==l:
+                    loc.append(i)
+                    loc.append(k)
+                    return loc
+    print("PLAIN TEXT:",end=' ')
+    i=0
+    while i<len(msg):
+        loc=list()
+        loc=locindex(msg[i])
+        loc1=list()
+        loc1=locindex(msg[i+1])
+        if loc[1]==loc1[1]:
+            print("{}{}".format(my_matrix[(loc[0]-1)%5][loc[1]],my_matrix[(loc1[0]-1)%5][loc1[1]]),end=' ')
+        elif loc[0]==loc1[0]:
+            print("{}{}".format(my_matrix[loc[0]][(loc[1]-1)%5],my_matrix[loc1[0]][(loc1[1]-1)%5]),end=' ')  
+        else:
+            print("{}{}".format(my_matrix[loc[0]][loc1[1]],my_matrix[loc1[0]][loc[1]]),end=' ')    
+        i=i+2  
+
     #Since we dont konw the  exact key at the moment we need
     #Boolean function to determine if its end of the row or 
     #the place value of the column
@@ -244,6 +381,13 @@ def menu_switch(args):
             answer = vigenereDec(rf.read(),args.Key)
             with open(args.Output, 'a') as wf:
                 wf.write(answer)
+
+    if (args.Cipher == 'PLF' and args.Type == 'ENC'):
+        with open(args.Input,'r') as rf:
+            answer = playfairEnc(rf.read(),args.Key)
+            with open(args.Output, 'a') as wf:
+                wf.write(answer)
+                
 
 if __name__ == '__main__':
     info()
