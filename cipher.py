7394a1c0c6dd516e7f4d4b7fb6fafc2d6df25b4e
kevin <hyunwoo777@csu.fullerton.edu>
2020-03-12 21:42:58 -0700

    VIG submitted

--- cipher.py
+++ cipher.py
@@ -354,6 +354,72 @@ def CeasDecrypt(ciphertext, key):
 
     return result
 
+def vigenereEnc(plaintext,key):
+    index = 0
+    i=0
+    #store all char values from the ASCII position
+    encString = []
+
+    for index in range(len(plaintext)):
+        #Gets the individual char from the plaintext and key strings
+        textPos = plaintext[index]
+        
+        lengthkey =len(key)
+        if i == lengthkey:
+            i=0
+            keyPos = key[i]
+        else:
+            keyPos = key[i]
+
+        #Look at ASCII position of the Plaintext + ASCII Position of the Key mod 26 because there are 26 letters in the alphabet 
+        posLetter = (ord(textPos) - 97 + ord(keyPos) - 97) % 26
+
+        #posLetter now gets its new position from the first ASCII value
+        posLetter += ord("a")
+
+        #we turn the ASCII value back to the char and add it to the list encString
+        encString.append(chr(posLetter))
+
+        i+=1
+
+    return ("".join(encString))
+
+########################################################################
+#Vigenere Decryption
+#Takes in a ciphertext string and key string and turns into dencrypted code
+########################################################################
+def vigenereDec(ciphertext,key):
+    index = 0
+    i=0
+    #store all char values from the ASCII position
+    decString = []
+    for index in range(len(ciphertext)):
+        #Gets the individual char from the plaintext and key strings
+        textPos = ciphertext[index]
+        lengthkey =len(key)
+        if i == lengthkey:
+            i=0
+            keyPos = key[i]
+        else:
+            keyPos = key[i]
+
+        #Look at ASCII position of the Plaintext - ASCII Position of the Key mod 26 because there are 26 letters in the alphabet
+        posLetter = ((ord(textPos) - 97) - (ord(keyPos) - 97) + 26) % 26
+	
+        #print((ord(textPos) - 97) - (ord(keyPos) - 97), end=" ")
+        #print(keyPos)
+	
+		
+        #print(posLetter)
+        #posLetter now gets its position from the first ASCII value
+        posLetter += ord("a")
+
+        #we turn the ASCII value back to the char and add it to the list deccString
+        decString.append(chr(posLetter))
+        i+= 1
+    return ("".join(decString))
+    
+
 
 def menu_switch(args):
     if (args.Cipher == 'RFC' and args.Type == 'ENC'):
@@ -410,6 +476,22 @@ def menu_switch(args):
         with open (args.Output, 'a') as wf:
                 wf.write(answer)
                 return (wf.name)
+                
+    if (args.Cipher == 'VIG' and args.Type == 'ENC'):
+       with open(args.Input, 'r') as rf:
+           answer = vigenereEnc(rf.read(), args.Key)
+           with open (args.Output, 'a') as wf:
+               wf.write(answer)  
+               return(wf.name)
+
+
+    if (args.Cipher == 'VIG' and args.Type == 'DEC'):
+        with open(args.Input,'r') as rf:
+            answer = vigenereDec(rf.read(),args.Key)
+            with open(args.Output, 'a') as wf:
+                wf.write(answer)
+                return(wf.name)
+
 
 
 
